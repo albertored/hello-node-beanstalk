df254732e89a08278148a2c760842223c8931d2c
Alberto Sartori <alberto.sartori@athonet.com>
2020-03-18 12:49:39 +0100

    prova pipeline

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 var cors = require('cors');
 
 var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3000;
+	app = express(),
+	port = process.env.PORT || 3000;
 
 app.use(cors())
 
 
-app.get('/', function(req, res){
-  res.json([{
-    "id": 1,
-    "title": "Questo è un titolo"
-  }])
+app.get('/', function (req, res) {
+	res.json([
+		{
+			"id": 1,
+			"title": "Questo è un titolo"
+		},
+		{
+			"id": 2,
+			"title": "pippo"
+		},
+		{
+			"id": 3,
+			"title": "Aggiunto da pipeline"
+		}
+	])
 })
 
-app.listen(port, function(){
-  console.log('Server listening on ', port);
+app.listen(port, function () {
+	console.log('Server listening on ', port);
 })
